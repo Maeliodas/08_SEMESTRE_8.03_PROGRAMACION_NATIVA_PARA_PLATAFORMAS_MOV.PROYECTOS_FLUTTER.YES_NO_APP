6afc858fdec4c342f8a67efeef91c688387f142b
VICTOR_ITURBIDE <adrianagreste18@gmail.com>
2026-03-25 10:39:05 -0600

    AÑADIMOS COLORES A LOS MENSAJES

--- lib/presentation/widgets/chat/my_message_bubble.dart
+++ lib/presentation/widgets/chat/my_message_bubble.dart
@@ -10,6 +10,10 @@ class MyMessageBubble extends StatelessWidget {
     return Column(
       children: [
         Container(
+          /*height: 10,
+          width: 10,*/
+          decoration: BoxDecoration(
+            color: Colors.green,          ),
           child: Padding(
             padding: EdgeInsets.all(8.0),
             child: Text('Incididunt aliquip labore ipsum velit do proident esse ipsum.'),
