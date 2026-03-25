70b6feed8b0d677666961079a507ef1a3b3f5ee4
VICTOR_ITURBIDE <adrianagreste18@gmail.com>
2026-03-25 10:51:40 -0600

    CODIGO DEL ARCHIVO HER (CAMBIA EL NOMBRE DE LA CLASE POR EL DEL ARCHIVO)

---
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HerMessageBubble extends StatelessWidget {
+  const HerMessageBubble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Container(
+          //height: 10,
+          width: 300,
+          decoration: BoxDecoration(
+            color: colors.primary,
+            borderRadius: BorderRadius.circular(20),
+            ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20.0,
+              vertical: 10.0
+            ),
+            child: Text('Incididunt aliquip labore ipsum velit do proident esse ipsum.',
+            style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        const SizedBox(height: 10,),
+        ],
+    );
+  }
+}
