40490de8e8270c1f6b0d672bad2d5d9eafbcfa9a
VICTOR_ITURBIDE <adrianagreste18@gmail.com>
2026-03-25 10:41:35 -0600

    USAMOS EL COLOR DEL TEMA DE LA APP

--- lib/presentation/widgets/chat/my_message_bubble.dart
+++ lib/presentation/widgets/chat/my_message_bubble.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 
 class MyMessageBubble extends StatelessWidget {
-
-
   const MyMessageBubble({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
     return Column(
       children: [
         Container(
           /*height: 10,
           width: 10,*/
           decoration: BoxDecoration(
-            color: Colors.green,          ),
+            color: colors.primary,
+            ),
           child: Padding(
             padding: EdgeInsets.all(8.0),
             child: Text('Incididunt aliquip labore ipsum velit do proident esse ipsum.'),
